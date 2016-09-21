1ebc37bc4751d882887c0baed1828651bfe8b946
Kazuyuki Saka <kazuyuki.saka@genies.jp>
2016-09-21 10:47:53 +0900

    設定ファイルのプロトコル取得ロジックを修正

--- fegg/settings.php
+++ fegg/settings.php
@@ -6,7 +6,7 @@
  * ほぼ変更のない値はdefineで定数定義し、アプリケーション毎に見直す必要がある値は$settingsとして変数定義している。
 
  * @author Genies, Inc.
- * @version 1.0.2
+ * @version 1.0.3
  */
 // 定数定義（基本的に変更の必要がない値を定義）
 // 制御
@@ -15,7 +15,11 @@ define('FEGG_DEFAULT_TIMEZONE', 'Asia/Tokyo');
 
 if (defined('FEGG_REWRITEBASE')) {
     // ドメイン
-    define('FEGG_CURRENT_PROTOCOL', isset($_SERVER['X-SSL-REQUEST']) && $_SERVER['X-SSL-REQUEST'] == 'on' ? 'https' : 'http');
+    if (isset($_SERVER['HTTPS'])) {
+        define('FEGG_CURRENT_PROTOCOL', $_SERVER['HTTPS'] == 'on' ? 'https' : 'http');
+    } else {
+        define('FEGG_CURRENT_PROTOCOL', isset($_SERVER['X-SSL-REQUEST']) && $_SERVER['X-SSL-REQUEST'] == 'on' ? 'https' : 'http');
+    }
     define('FEGG_CURRENT_PORT', in_array( $_SERVER['SERVER_PORT'], array( '80', '443' ) ) ? '' : $_SERVER['SERVER_PORT']);
     define('FEGG_UNIQUE_SCHEME', (isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'dummy.com').(FEGG_CURRENT_PORT ? ':'.FEGG_CURRENT_PORT : ''));
     define('FEGG_CURRENT_DOMAIN', FEGG_CURRENT_PROTOCOL . '://' . FEGG_UNIQUE_SCHEME);
